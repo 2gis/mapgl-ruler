18586b73686efb51091d33b6ced72b9f0ed4d085
Dmitriy Gordienko <d.gordienko@2gis.ru>
2022-03-18 14:51:30 +0300

    f

--- src/utils.ts
+++ src/utils.ts
@@ -150,17 +150,17 @@ export function getLabel(
     map: mapgl.Map,
     point: GeoPoint,
     text: string,
-    interactive = false,
+    isPopUp = false,
 ): mapgl.HtmlMarker {
     const content = getLabelHtml(text, styles.labelFontSize);
     const height = styles.labelFontSize;
     const jointTotalWidth = styles.jointWidth + styles.jointBorderWidth + styles.jointBorder2Width;
-
+    const interactive = isPopUp;
     return new mapgl.HtmlMarker(map, {
         coordinates: point,
         html: content,
         anchor: [-(jointTotalWidth / 2 + 2), height / 2],
-        zIndex: styles.linePhase + 1,
+        zIndex: isPopUp ? styles.popupLabelPhase : styles.linePhase + 1,
         interactive,
     });
 }
