e74ff43904f8a6f4f5839210d55095cd7d316c18
Dmitriy Gordienko <d.gordienko@2gis.ru>
2022-05-11 15:33:51 +0300

    f

--- jest.config.js
+++ jest.config.js
@@ -8,7 +8,7 @@ const suiteParams = {
         maxConcurrency: 3,
         testTimeout: 15000,
         testEnvironment: 'node',
-        testMatch: ['**/*/ruler_control.screen.ts'],
+        testMatch: ['**/*/*.screen.ts'],
     },
     unit: {
         testEnvironment: 'node',
