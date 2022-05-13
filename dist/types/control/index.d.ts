be5dc63d76c632a2ead5365b1ea29f41e6753f86
Dmitriy Gordienko <d.gordienko@2gis.ru>
2022-05-13 17:24:33 +0300

    f

--- dist/types/control/index.d.ts
+++ dist/types/control/index.d.ts
@@ -1,10 +1,21 @@
-import { Ruler } from '../ruler';
+import { Ruler, RulerOptions } from '../ruler';
+export interface RulerControlOptions extends mapgl.ControlOptions {
+    /**
+     * Sets ruler's behavior. Specifies whether the ruler should be in measuring mode of the distance of a polyline or the area of a polygon.
+     */
+    mode?: RulerOptions['mode'];
+    /**
+     * Specifies whether the ruler should be enabled after control initialization.
+     */
+    enabled?: boolean;
+}
 /**
- * Provide ruler control on map.
+ * A class that provides a ruler control on the map.
  */
 export declare class RulerControl extends mapgl.Control {
     private map;
     private readonly ruler;
+    private readonly icon;
     private isEnabled;
     /**
      * Example:
@@ -17,16 +28,19 @@ export declare class RulerControl extends mapgl.Control {
      * ]);
      * ```
      * @param map The map instance.
-     * @param options Control initialization options.
-     * @param enabled Enable on create.
+     * @param options Ruler control initialization options.
      */
-    constructor(map: mapgl.Map, options: mapgl.ControlOptions, enabled?: boolean);
+    constructor(map: mapgl.Map, options: RulerControlOptions);
     /**
-     * Destroy control and ruler
+     * Destroys the control and the ruler.
      */
     destroy(): void;
     /**
-     * Get Ruler object
+     * Returns the ruler instance.
      */
     getRuler(): Ruler;
+    /**
+     * Toggle control. The same as clicking the button.
+     */
+    toggle(): void;
 }
