d7b3eff38f25e86b3d5544cbf592111fa50ef7a2
Oleg Kalyanov <o.kalyanov@flamp.ru>
2022-06-22 16:28:07 +0700

    Make ruler mode optional (#18)
    * Make ruler mode optional
    
    * 2.0.1
    
    * fix

--- src/utils.ts
+++ src/utils.ts
@@ -2,10 +2,8 @@ import { GeoPoint, ScreenPoint, SnapInfo } from './types';
 import { Joint } from './joint';
 import { dictionary } from './l10n';
 import { style } from './style';
+import { JOINT_REMOVE_BUTTON_SVG } from './constants';
 import css from './index.module.css';
-
-const jointRemoveBtnSvg =
-    'PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHhtbG5zOnhsaW5rPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5L3hsaW5rIiB3aWR0aD0iMjIiIGhlaWdodD0iMjIiPjxkZWZzPjxwYXRoIGlkPSJBIiBkPSJNMTIgMTAuNTg2bDMuNzkzLTMuNzkzIDEuNDE0IDEuNDE0TDEzLjQxNCAxMmwzLjc5MyAzLjc5My0xLjQxNCAxLjQxNEwxMiAxMy40MTRsLTMuNzkzIDMuNzkzLTEuNDE0LTEuNDE0TDEwLjU4NiAxMiA2Ljc5MyA4LjIwN2wxLjQxNC0xLjQxNEwxMiAxMC41ODZ6Ii8+PC9kZWZzPjxnIGZpbGwtcnVsZT0iZXZlbm9kZCIgdHJhbnNmb3JtPSJ0cmFuc2xhdGUoLTEgLTEpIj48Y2lyY2xlIGN4PSIxMiIgY3k9IjEyIiByPSIxMC41IiBmaWxsPSIjZmZmIiBzdHJva2U9IiMwMDAiIHN0cm9rZS1vcGFjaXR5PSIuMTUiLz48bWFzayBpZD0iQiIgZmlsbD0iI2ZmZiI+PHVzZSB4bGluazpocmVmPSIjQSIvPjwvbWFzaz48dXNlIGZpbGw9IiMwMDAiIGZpbGwtcnVsZT0ibm9uemVybyIgeGxpbms6aHJlZj0iI0EiLz48ZyBmaWxsPSIjMjYyNjI2IiBtYXNrPSJ1cmwoI0IpIj48cGF0aCBkPSJNMCAwaDI0djI0SDB6Ii8+PC9nPjwvZz48L3N2Zz4=';
 
 /**
  * @hidden
@@ -85,7 +83,7 @@ export function getSnapLabelHtml(firstLine: string, secondLine: string): string 
  * @internal
  */
 export function getMarkerPopupHtml(): string {
-    return `<img class=${css.cross} src="data:image/svg+xml;base64,${jointRemoveBtnSvg}" alt="close">`;
+    return `<img class=${css.cross} src="data:image/svg+xml;base64,${JOINT_REMOVE_BUTTON_SVG}" alt="close">`;
 }
 
 /**
