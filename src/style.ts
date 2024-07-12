60b396f70b90eb8c0077836a21610f838064f080
nortren <nortren@inbox.ru>
2024-07-12 17:07:24 +0300

    https://jira.2gis.ru/browse/TILES-6537 fix after review

--- src/style.ts
+++ src/style.ts
@@ -1,4 +1,8 @@
-const maxPhase = 10; // an even number that allows us not to go to a negative value when calculating elements.
+// The z-index value for this element is set to 10. This even number is chosen to be lower than the z-index
+// of controls and plugins in MapGL, which is 50. This approach allows us to manage the layering of elements
+// without resorting to negative z-index values.
+
+const maxPhase = 10;
 
 /**
  * @hidden
